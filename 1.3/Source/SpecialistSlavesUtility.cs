34e602ff8bd29a14d7b58563b90034a68c37b903
LordSicarious <87661780+LordSicarious@users.noreply.github.com>
2022-10-12 14:41:09 +1100

    Bug fix
    Previously caused errors when assessing thought states on roles without role effects

--- 1.3/Source/SpecialistSlavesUtility.cs
+++ 1.3/Source/SpecialistSlavesUtility.cs
@@ -9,8 +9,8 @@ public static class SpecialistSlavesUtility {
 
     public static Texture2D WillingSlaveSuppressionFillTex = SolidColorMaterials.NewSolidColorTexture(new Color32(50, 205, 80, byte.MaxValue));
     public static bool IsWillingSlave(this Pawn pawn) {
-        Precept_Role role = pawn.ideo?.Ideo.GetRole(pawn);
-        if(role != null && role.def.roleEffects.Any(effect => effect as RoleEffect_WillingSlave != null)) {
+        Precept_Role role = pawn.ideo?.Ideo?.GetRole(pawn);
+        if(role != null && (role.def?.roleEffects?.Any(effect => effect as RoleEffect_WillingSlave != null) ?? false)) {
             return true;
         }
         return false;
