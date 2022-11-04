4a84980a66a67b0a99d8a23b8aafeeccaa4d0753
LordSicarious <87661780+LordSicarious@users.noreply.github.com>
2022-11-05 04:49:49 +1100

    Delete 1.4 directory

--- 1.4/Source/SpecialistSlaves_ModExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿// SpecialistSlaves.SpecialistSlaves_ModExtension
-using RimWorld;
-using Verse;
-
-namespace SpecialistSlaves {
-public class SpecialistSlaves_ModExtension : DefModExtension {
-    public SlaveStatusRequirement slavePermitted = SlaveStatusRequirement.Forbidden;
-}
-}
